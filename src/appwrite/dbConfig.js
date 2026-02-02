bf60c5db2a96891fb87a72a0d851b4740e1b88f0
Dipak Kumar Chauhan <ece22133@iiitkalyani.ac.in>
2026-02-02 19:14:39 +0530

    Initial configuration done, Appwrite initialization and configuration for authentication, database setup and upload done.

---
+++ src/appwrite/dbConfig.js
@@ -0,0 +1,156 @@
+import conf from "../config/config.js";
+import { Client,ID, Databases,Storage,Query } from "appwrite";
+
+
+export class DBService {
+
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor() {
+        his.client
+            .setEndpoint(conf.APPWRITE_URL) // Your Appwrite Endpoint
+            .setProject(conf.APPWRITE_PROJECT_ID); // Your project ID
+        
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+
+
+    }
+
+    async createPost({
+        title, slug, content,featuredImage,status,userId
+    }){
+
+        try {
+            return await this.databases.createDocument(
+                conf.APPWRITE_DATABASE_ID,
+                conf.APPWRITE_COLLECTION_ID,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+            
+        } catch (error) {
+            console.log("Appwrite DB Create Post Error ", error);
+        }
+    }
+
+    async updatePost(slug,{
+        title,  content,featuredImage,status
+    }){
+        try {
+            
+            return await this.databases.updateDocument(
+                conf.APPWRITE_DATABASE_ID,
+                conf.APPWRITE_COLLECTION_ID,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    
+                }
+            )
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async deletePost(slug){
+        try {
+            
+            await this.databases.deleteDocument(
+                conf.APPWRITE_DATABASE_ID,
+                conf.APPWRITE_COLLECTION_ID,
+                slug
+            )
+            return true;
+        } catch (error) {
+            throw error;
+            return false;
+        }
+    }
+
+    async getPosts(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.APPWRITE_DATABASE_ID,
+                conf.APPWRITE_COLLECTION_ID,
+                slug
+            )
+            
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getAllPosts(queries = [Query.equal("status","active")]){
+        try {
+            
+            return await this.databases.listDocuments(
+                conf.APPWRITE_DATABASE_ID,
+                conf.APPWRITE_COLLECTION_ID,
+                queries  
+            )
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    // // File Upload Services
+    // async uploadFile(file){
+    //     try {
+    //         return await this.bucket.createFile(
+    //             conf.APPWRITE_BUCKET_ID,
+    //             ID.unique(),
+    //             file
+    //         )
+            
+    //     } catch (error) {
+    //         throw error;
+    //     }
+    // }
+
+    // async deleteFile(fileId){
+    //     try {
+    //         return await this.bucket.deleteFile(
+    //             conf.APPWRITE_BUCKET_ID,
+                
+    //             fileId
+    //         )
+
+    //     } catch (error) {
+    //         throw error;
+    //     }
+    // }
+
+    // async getFilePreview(fileId){
+    //     try {
+
+    //         return await this.bucket.getFilePreview(
+    //             conf.APPWRITE_BUCKET_ID,
+    //             fileId
+    //         )
+            
+    //     } catch (error) {
+    //         throw error;
+    //     }
+    // }
+
+}
+
+const dbservice = new DBService();
+
+export default dbservice;
+
+
---
+++ src/appwrite/upload.js
@@ -0,0 +1,64 @@
+import conf from "../config/config.js";
+import { Client,ID,Storage} from "appwrite";
+
+export class UploadService {
+
+    client = new Client();
+   
+    bucket;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.APPWRITE_URL) // Your Appwrite Endpoint
+            .setProject(conf.APPWRITE_PROJECT_ID); // Your project ID
+        
+        this.bucket = new Storage(this.client);
+
+
+    }
+
+     // File Upload Services
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.APPWRITE_BUCKET_ID,
+                ID.unique(),
+                file
+            )
+            
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            return await this.bucket.deleteFile(
+                conf.APPWRITE_BUCKET_ID,
+                
+                fileId
+            )
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getFilePreview(fileId){
+        try {
+
+            return await this.bucket.getFilePreview(
+                conf.APPWRITE_BUCKET_ID,
+                fileId
+            )
+            
+        } catch (error) {
+            throw error;
+        }
+    }
+
+}
+
+const uploadService = new UploadService();
+
+export default uploadService;
